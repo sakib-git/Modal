7b9da64df89f977923fca2d9dddb3ba3460165f6
sakib-git <sakibulhasan32002@gmail.com>
2025-12-03 18:17:02 +0600

    f

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Modal = () => {
   const [modal, setModal] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('data.json')
+      .then((res) => res.json())
+      .then((d) => {
+        setData(d);
+      });
+  }, []);
+
+  const handlemoda = (p) => {
+    setModal(true);
+    console.log('puki',p)
+ 
+  };
   return (
     <div>
       {modal && (
@@ -12,15 +26,28 @@ const Modal = () => {
                 X
               </button>
             </div>
-            <div className='p-5 font-semibold'>
-              <h1 className='text-2xl'>!Hello</h1>
-              <p>This is a modal</p>
+            <div className="p-5 font-semibold">
+              {/* <h1 className='text-2xl'>!Hello</h1>
+              <p>This is a modal</p> */}
+              {data.map((s) => (
+                <div key={s.name}>
+                  <h1 className="text-2xl">Name: {s.name}</h1>
+                  <p className="text-2xl">Age: {s.age}</p>
+                  <p>Student: {s?.isStudent ? 'Yes' : 'No'}</p>
+
+                  <div className='flex gap-2'>
+                    {s.skills.map((skill, i) => (
+                   <p key={i}>{skill}</p>
+                  ))}
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       )}
 
-      <button onClick={() => setModal(true)} className="bg-base-100 px-4 py-2 rounded-md text-2xl  shadow font-bold mr-10 mt-20">
+      <button onClick={() => handlemoda(data)} className="bg-base-100 px-4 py-2 rounded-md text-2xl  shadow font-bold mr-10 mt-20">
         Open Modal
       </button>
     </div>
